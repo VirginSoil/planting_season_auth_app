c7d1f4e8f7414e2cc0211414d1d5fcce7111896d
Luke Martinez <lukemartinez@gmail.com>
2014-01-27 17:46:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlantingSeasonAuth::Application.config.secret_key_base = 'd333ca46b19ee9fa697ccca6af610d9bde781eaad123b5632029f0db39aa2f8d53dc928f24ad3d6d7836fb818bf430200bd8ca7f27a0a7fc605203370b077919'
