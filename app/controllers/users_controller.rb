c8395cf805d5e4ac34d5357d19ab82d550297c99
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-28 17:30:12 -0700

    add user params and create action on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to gardens_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :full_name, :email, :phone, :password, :password_confirmation)
+  end
 end
