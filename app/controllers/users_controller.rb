0f87ed3f36d3e653fb8217bc1c134472ed6219d2
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-28 17:46:00 -0700

    successfully sign up a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Thanks, #{@user.full_name}, for Signing Up!"
       redirect_to gardens_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ PlantingSeasonAuth::Application.routes.draw do
   root "home#index"
   resources :users
   get "/signup", to: "home#login_signup"
+  get "/gardens", to: redirect("/")
 end
