53d6c6f3cb85fa510c555da9978bdf7196b2620e
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-29 10:42:42 -0700

    add user login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,15 @@ class HomeController < ApplicationController
   def login_signup
     @user = User.new
   end
+
+  def login
+    user = User.find_by(:email => params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      flash[:success] = "Welcome back, #{user.full_name}!"
+      redirect_to gardens_path
+    else
+      flash[:error] = "Email or Password Incorrect"
+      redirect :back
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ PlantingSeasonAuth::Application.routes.draw do
   root "home#index"
   resources :users
   get "/signup", to: "home#login_signup"
+  get "/login",  to: "home#login_signup"
+  post "/login", to: "home#login"
   get "/gardens", to: redirect("/")
 end
