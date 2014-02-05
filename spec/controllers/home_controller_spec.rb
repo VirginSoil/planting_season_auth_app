c6f2594b60bcd3135c8f2fe4a3ac8f864a702203
Ben Lewis <bennlewis@gmail.com>
2014-02-05 13:36:27 -0700

    update specs to reflect new routing

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe HomeController do
 
-  describe "#index" do
-    it "should return a status 200" do
-      get :index
-      expect(response).to be_success
-    end
-  end
-
   describe "#login_signup" do
     it "should return a status 200" do
       get :login_signup
@@ -18,7 +11,7 @@ describe HomeController do
   end
 
   describe "#login" do
-    it "should login a user with correct credentials" do 
+    it "should login a user with correct credentials" do
       create(:user)
       post :login, { :user => { :email => "tyler.stephen.long@gmail.com",
                                 :password => "password" } }
@@ -27,7 +20,7 @@ describe HomeController do
       expect(response).to be_redirect
     end
 
-    it "should redirect users back who have invalid credentials" do 
+    it "should redirect users back who have invalid credentials" do
       request.env["HTTP_REFERER"] = login_path
       create(:user)
       post :login, { :user => { :email => "tyler.stephen.long@gmail.com",
