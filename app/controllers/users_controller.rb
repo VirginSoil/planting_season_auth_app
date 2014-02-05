c380d2731bc182dcc8106a58034ed831174c39ee
Ben Lewis <bennlewis@gmail.com>
2014-02-05 13:42:39 -0700

    update controller action to render the correct page on failure and add error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Thanks, #{@user.full_name}, for Signing Up!"
       redirect_to gardens_path
     else
-      render :new
+      flash.notice = "There was an error with your
+        information. Please ensure all of the required fields are filled out."
+      render 'home/login_signup'
     end
   end
 
