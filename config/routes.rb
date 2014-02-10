9c65aea7f6d25e34f6a8d499f0bfa42938647647
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-10 13:15:23 -0700

    scope to /auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 PlantingSeasonAuth::Application.routes.draw do
-  root "home#login_signup"
-  resources :users
-  get "/signup", to: "home#login_signup"
-  get "/login",  to: "home#login_signup"
-  post "/login", to: "home#login"
-  get "/gardens", to: redirect("/")
+  scope "auth" do
+    root "home#login_signup"
+    resources :users
+    get "/signup", to: "home#login_signup"
+    get "/login",  to: "home#login_signup"
+    post "/login", to: "home#login"
+    get "/gardens", to: redirect("/")
+  end
 end
